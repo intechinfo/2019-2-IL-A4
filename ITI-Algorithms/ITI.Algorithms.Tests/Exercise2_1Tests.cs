8a7ba30afae3a305af22fe139243c00a1d4bcb2c
Antoine Raquillet <antoine.raquillet@esiea.fr>
2020-01-17 16:03:23 +0100

    Added ITI-Algorithms.

---
+++ ITI-Algorithms/ITI.Algorithms.Tests/Exercise2_1Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace ITI.Algorithms.Tests
+{
+    [TestFixture]
+    public class Exercise2_1Tests
+    {
+        [TestCase("abc", false)]
+        [TestCase("aaa", true)]
+        [TestCase("abb", true)]
+        [TestCase("cdefghc", true)]
+        public void exercise1_works(string input, bool expected)
+        {
+            Assert.That(Exercise2_1.ContainsDuplicateChar(input), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ ITI-Algorithms/ITI.Algorithms/Exercise2_1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ITI.Algorithms
+{
+    public class Exercise2_1
+    {
+        public static bool ContainsDuplicateChar(string s)
+        {
+            HashSet<char> uniqueChars = new HashSet<char>();
+            foreach (char c in s)
+            {
+                if (!uniqueChars.Add(c)) return true;
+            }
+
+            return false;
+        }
+    }
+}
